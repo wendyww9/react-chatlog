c570d1e49cb0e75d2ecfde8471715a3ced5e778f
wendyww9 <wendyskystar@gmail.com>
2025-06-10 00:11:53 -0700

    Import ChatLog and pass messages to ChatLog

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css';
+import chatMessages from './data/messages.json';
+import ChatLog from './components/ChatLog.jsx';
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
+        <section>
+          <p>22</p>
+        </section>
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatLog entries={chatMessages}></ChatLog>
       </main>
     </div>
   );
